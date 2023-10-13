912a2de80d941a44be14dbddc210e8af1df87e74
Ivhene <ivarh.nesheim@gmail.com>
2023-10-13 14:27:31 +0200

    feat: made the accountSelect component

---
+++ components/AccountSelect.tsx
@@ -0,0 +1,19 @@
+import { AccountSelectParams } from "@/types/paramTypes";
+import Link from "next/link";
+
+export default function AccountSelect({ accounts }: AccountSelectParams) {
+  const showAccounts = () => {
+    return accounts.map((account) => {
+      return (
+        <Link href={`/${account.accountID}`}>
+          <div>
+            <p>{account.accountID}</p>
+            <p>{account.accountName}</p>
+          </div>
+        </Link>
+      );
+    });
+  };
+
+  return <div>{showAccounts()}</div>;
+}
