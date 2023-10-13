02b8482239f888f196483ebe45a3066fb76d36b9
598990 <598990@stud.hvl.no>
2023-10-13 22:24:40 +0200

    fix: updated url with env variable

--- app/(accounts)/[id]/page.tsx
+++ app/(accounts)/[id]/page.tsx
@@ -3,7 +3,9 @@ import { AccountIdParams } from "@/types/paramTypes";
 export default async function ShowAccount({ params }: AccountIdParams) {
   const id = parseInt(params.id);
 
-  const response = await fetch(`http://localhost:3000/api/accounts/${id}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/accounts/${id}`
+  );
 
   const account = await response.json();
 
