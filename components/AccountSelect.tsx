e070c730dfb37c241a50a50720442147ffc60316
598990 <598990@stud.hvl.no>
2023-10-13 23:18:11 +0200

    fix: added key to accounts mapping

--- components/AccountSelect.tsx
+++ components/AccountSelect.tsx
@@ -5,7 +5,7 @@ export default function AccountSelect({ accounts }: AccountSelectParams) {
   const showAccounts = () => {
     return accounts.map((account) => {
       return (
-        <Link href={`/${account.accountID}`}>
+        <Link key={account.accountID} href={`/${account.accountID}`}>
           <div>
             <p>{account.accountID}</p>
             <p>{account.accountName}</p>
