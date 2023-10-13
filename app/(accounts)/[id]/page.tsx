29e7616f2276230bd988a5a1eb6e0e15985b62ea
598990 <598990@stud.hvl.no>
2023-10-13 22:02:13 +0200

    feat: gets account from response

--- app/(accounts)/[id]/page.tsx
+++ app/(accounts)/[id]/page.tsx
@@ -3,7 +3,9 @@ import { AccountIdParams } from "@/types/paramTypes";
 export default async function ShowAccount({ params }: AccountIdParams) {
   const id = parseInt(params.id);
 
-  const response = fetch(`http://localhost:3000/api/accounts/${id}`);
+  const response = await fetch(`http://localhost:3000/api/accounts/${id}`);
+
+  const account = await response.json();
 
   return <div>{id}</div>;
 }
