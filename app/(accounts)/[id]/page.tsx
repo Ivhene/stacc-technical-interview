63f2c2ca8f842a9c04ac6d0c1afd19e5506147e6
598990 <598990@stud.hvl.no>
2023-10-13 21:58:22 +0200

    feat: fetches account with id

--- app/(accounts)/[id]/page.tsx
+++ app/(accounts)/[id]/page.tsx
@@ -1,7 +1,9 @@
 import { AccountIdParams } from "@/types/paramTypes";
 
 export default async function ShowAccount({ params }: AccountIdParams) {
-  const id = params.id;
+  const id = parseInt(params.id);
+
+  const response = fetch(`http://localhost:3000/api/accounts/${id}`);
 
   return <div>{id}</div>;
 }
