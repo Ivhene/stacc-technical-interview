2c2d612cd0beefcfe40bf506983b4b65a9b4a05f
598990 <598990@stud.hvl.no>
2023-10-14 11:47:01 +0200

    feat: structure styling

--- components/showAccount/AccountView.tsx
+++ components/showAccount/AccountView.tsx
@@ -1,8 +1,8 @@
 export default function AccountView() {
   return (
-    <div>
-      <div>Box 1</div>
-      <div>Box 2</div>
+    <div className="fixed inset-0 bg-red-100 flex flex-row items-center justify-center space-x-2">
+      <div className="bg-red-50 w-1/3 h-3/4"></div>
+      <div className="bg-red-50 w-1/3 h-3/4">Box 2</div>
     </div>
   );
 }
