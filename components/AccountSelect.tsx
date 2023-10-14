8e0763a02dfa501c03ac2ad61b48af874ef80f52
598990 <598990@stud.hvl.no>
2023-10-14 11:07:49 +0200

    feat: added styling to accountSelect

--- components/AccountSelect.tsx
+++ components/AccountSelect.tsx
@@ -6,7 +6,7 @@ export default function AccountSelect({ accounts }: AccountSelectParams) {
     return accounts.map((account) => {
       return (
         <Link key={account.accountID} href={`/${account.accountID}`}>
-          <div>
+          <div className="h-[100px] rounded-3xl text-center bg-amber-100 border border-amber-400">
             <p>{account.accountID}</p>
             <p>{account.accountName}</p>
           </div>
@@ -15,5 +15,11 @@ export default function AccountSelect({ accounts }: AccountSelectParams) {
     });
   };
 
-  return <div>{showAccounts()}</div>;
+  return (
+    <div className="fixed inset-0 bg-amber-100 flex justify-center items-center">
+      <div className="bg-amber-50 text-neutral-800 grid grid-cols-5 w-[80%] rounded-3xl p-10 items-center gap-8 border border-amber-400">
+        {showAccounts()}
+      </div>
+    </div>
+  );
 }
