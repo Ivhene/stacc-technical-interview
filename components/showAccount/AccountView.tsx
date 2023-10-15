ab2f096c72b7fdec31acd74f29ca6904cc1a5bb9
598990 <598990@stud.hvl.no>
2023-10-15 22:29:31 +0200

    fix: changed colors from red to amber

--- components/showAccount/AccountView.tsx
+++ components/showAccount/AccountView.tsx
@@ -39,33 +39,33 @@ export default function AccountView({ account }: ShowAccountParams) {
   );
 
   return (
-    <div className="fixed inset-0 bg-red-100 flex flex-col md:flex-row items-center justify-center md:space-x-2 text-[14px] p-7 md:p-0 space-y-2">
-      <div className="bg-red-50 h-1/2 lg:w-1/3 xl:w-1/4 md:w-2/5 md:h-3/4 rounded-3xl p-10 flex flex-col space-y-4 overflow-auto">
-        <div className="bg-red-100 h-[100px] p-5 rounded-3xl flex flex-col justify-center items-center space-y-4 border-2 border-red-400">
+    <div className="fixed inset-0 bg-amber-100 flex flex-col md:flex-row items-center justify-center md:space-x-2 text-[14px] p-7 md:p-0 space-y-2">
+      <div className="bg-amber-50 h-1/2 lg:w-1/3 xl:w-1/4 md:w-2/5 md:h-3/4 rounded-3xl p-10 flex flex-col space-y-4 overflow-auto">
+        <div className="bg-amber-100 h-[100px] p-5 rounded-3xl flex flex-col justify-center items-center space-y-4 border-2 border-amber-400">
           <p>{account.accountName}</p>
           <p className="font-bold">{account.balance} kr</p>
         </div>
         {nextMilestone && (
-          <div className="bg-red-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-red-400 text-[10px]  md:text-[12px]">
+          <div className="bg-amber-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-amber-400 text-[10px]  md:text-[12px]">
             <div className="flex flex-row justify-between w-full">
               <p>{`Next Milestone - ${nextMilestone?.goalTarget} kr`}</p>
               <p>{progressValueMilestone.toFixed(1)}%</p>
             </div>
             <Progress
               value={progressValueMilestone}
-              className="bg-red-50 border-2 border-red-400"
+              className="bg-amber-50 border-2 border-amber-400"
             />
           </div>
         )}
         {nextGoal && (
-          <div className="bg-red-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-red-400 text-[10px] md:text-[12px]">
+          <div className="bg-amber-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-amber-400 text-[10px] md:text-[12px]">
             <div className="flex flex-row justify-between w-full">
               <p>{`${nextGoal?.goalName} - ${nextGoal?.goalTarget} kr`}</p>
               <p>{progressValueGoal.toFixed(1)}%</p>
             </div>
             <Progress
               value={progressValueGoal}
-              className="bg-red-50 border-2 border-red-400"
+              className="bg-amber-50 border-2 border-amber-400"
             />
           </div>
         )}
--- components/showAccount/UpcomingAndReachedGoals.tsx
+++ components/showAccount/UpcomingAndReachedGoals.tsx
@@ -14,7 +14,7 @@ export function UpcomingAndReachedGoals({
         return (
           <div
             key={goal.goalID}
-            className="bg-red-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-red-400  text-[10px] md:text-[12px]"
+            className="bg-amber-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-amber-400  text-[10px] md:text-[12px]"
           >
             <div className="flex flex-row justify-between w-full">
               <p>{`${goal.goalName} - ${goal.goalTarget} kr`}</p>
@@ -22,7 +22,7 @@ export function UpcomingAndReachedGoals({
             </div>
             <Progress
               value={progressValue}
-              className="bg-red-50 border-2 border-red-400"
+              className="bg-amber-50 border-2 border-amber-400"
             />
           </div>
         );
@@ -36,7 +36,7 @@ export function UpcomingAndReachedGoals({
         return (
           <div
             key={goal.goalID}
-            className="bg-red-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-red-400  text-[10px] md:text-[12px]"
+            className="bg-amber-100 h-[70px] p-3 rounded-3xl flex flex-col justify-center items-center border-2 border-amber-400  text-[10px] md:text-[12px]"
           >
             <div className="flex flex-row justify-between w-full">
               <p>{`${goal.goalName} - ${goal.goalTarget} kr`}</p>
@@ -48,14 +48,14 @@ export function UpcomingAndReachedGoals({
             </div>
             <Progress
               value={progressValue}
-              className="bg-red-50 border-2 border-red-400"
+              className="bg-amber-50 border-2 border-amber-400"
             />
           </div>
         );
       });
 
   return (
-    <div className="bg-red-50 h-1/2 xl:w-1/4 lg:w-1/3 md:w-2/5 md:h-3/4 rounded-3xl p-10 flex flex-col space-y-4 overflow-auto">
+    <div className="bg-amber-50 h-1/2 xl:w-1/4 lg:w-1/3 md:w-2/5 md:h-3/4 rounded-3xl p-10 flex flex-col space-y-4 overflow-auto">
       {upcomingDisplay()}
       {reachedDisplay()}
     </div>
