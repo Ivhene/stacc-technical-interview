0df572e797a3e83bb93f597deb7f7956a55c53ec
598990 <598990@stud.hvl.no>
2023-10-15 19:40:28 +0200

    feat: responsive css on AccountSelect

--- components/AccountSelect.tsx
+++ components/AccountSelect.tsx
@@ -12,7 +12,7 @@ export default function AccountSelect({ accounts }: AccountSelectParams) {
           href={`/${account.accountID}`}
           onClick={() => console.log("click")}
         >
-          <div className="h-[100px] rounded-3xl text-center bg-amber-100 border border-amber-400">
+          <div className="h-[75px] sm:h-[100px] rounded-3xl text-center bg-amber-100 border border-amber-400">
             <p>{account.accountID}</p>
             <p>{account.accountName}</p>
           </div>
@@ -23,7 +23,7 @@ export default function AccountSelect({ accounts }: AccountSelectParams) {
 
   return (
     <div className="fixed inset-0 bg-amber-100 flex justify-center items-center">
-      <div className="bg-amber-50 text-neutral-800 grid grid-cols-5 w-[80%] rounded-3xl p-10 items-center gap-8 border border-amber-400">
+      <div className="bg-amber-50 text-neutral-800 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 w-[80%] rounded-3xl p-10 items-center gap-8 border border-amber-400">
         {showAccounts()}
       </div>
     </div>
