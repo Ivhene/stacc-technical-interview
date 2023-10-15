54e19a964f63bef7286be6ac15f5e3eabd25ee6c
598990 <598990@stud.hvl.no>
2023-10-15 10:37:05 +0200

    fix: moved api call out of main function

--- app/(accounts)/[id]/page.tsx
+++ app/(accounts)/[id]/page.tsx
@@ -1,16 +1,18 @@
 import AccountView from "@/components/showAccount/AccountView";
 import { AccountIdParams } from "@/types/paramTypes";
 
-export default async function ShowAccount({ params }: AccountIdParams) {
-  const id = parseInt(params.id);
-
-  console.log("Check");
-
+async function getData(id: number) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/accounts/${id}`
   );
 
-  const data = await response.json();
+  return await response.json();
+}
+
+export default async function ShowAccount({ params }: AccountIdParams) {
+  const id = parseInt(params.id);
+
+  const data = await getData(id);
 
   return <AccountView account={data.account} />;
 }
